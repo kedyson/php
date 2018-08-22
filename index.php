9029ec1aa432373d41c815af4624dcdc8624aa3d
Kedyson Ferreira <kedysonf@yahoo.com.br>
2018-08-21 23:48:02 -0300

    resolvi o conflito de olas

--- index.php
+++ index.php
@@ -3,11 +3,6 @@
 <<<<<<< HEAD
 	echo "Olá Mundo 456!";
 
-	echo "Olá Udemy!";=
-=======
-	echo "Olá Mundo 123!";
-		
-	echo "Olá HCode!";
->>>>>>> 0686ebd9b567e7283451d57d1028060d904e91a3
+	echo "Olá Udemy e HCode!";
 
 ?>
